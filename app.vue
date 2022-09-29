62b8283e9f95f34c61a57ec1f4f851c1f585fafb
donmatatan <114635811+donmatatan@users.noreply.github.com>
2022-09-29 00:18:05 -0300

    Add files via upload

---
+++ app.vue
@@ -0,0 +1,153 @@
+<template>
+  <div>
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+      <meta charset="UTF-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>Document</title>
+      
+
+     
+      <link rel="stylesheet" href="CSS/style.css">
+      <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+    </head>
+    <body class="text-white bg-black" >
+     
+
+      <div class=" grid grid-cols-7 gap-x-20 gap-y-4 	 " role="group">
+
+        <div class="relative  mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0  material-symbols-outlined ">add</span>
+          <span class=" absolute inset-x-0 ">Add</span> 
+        </button>
+        </div>
+         <div class="relative  mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">delete</span>
+          <span class=" absolute inset-x-0 ">Delete</span> 
+        </button>
+
+         </div>
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">done</span>
+          <span class=" absolute inset-x-0 ">Validate</span> 
+        </button>
+
+         </div>
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">display_settings</span>
+          <span class=" absolute inset-x-0 ">Modify</span> 
+        </button>
+
+         </div>
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">add_location</span>
+          <span class=" absolute inset-x-0 ">Map</span> 
+        </button>
+
+         </div>
+         
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">assistant_direction</span>
+          <span class=" absolute inset-x-0 ">Type</span> 
+        </button>
+
+         </div>
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">clear_night</span>
+          <span class=" absolute inset-x-0 ">Night</span> 
+        </button>
+
+         </div>
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">add_to_queue</span>
+          <span class=" absolute inset-x-0 ">Screen</span> 
+        </button>
+
+         </div>
+
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">add_road</span>
+          <span class=" absolute inset-x-0 ">Road</span> 
+        </button>
+
+         </div>
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">crop_5_4</span>
+          <span class=" absolute inset-x-0 ">Frames</span> 
+        </button>
+
+         </div>
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">archive</span>
+          <span class=" absolute inset-x-0 ">Archive</span> 
+        </button>
+
+         </div>
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">search</span>
+          <span class=" absolute inset-x-0 ">Search</span> 
+        </button>
+
+         </div>
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">report</span>
+          <span class=" absolute inset-x-0 ">Report</span> 
+        </button>
+
+         </div>
+         <div class=" relative mb-4 ">
+          <button  type="button" >
+          <span class=" absolute inset-x-0 top-0 material-symbols-outlined ">share</span>
+          <span class=" absolute inset-x-0 ">Add</span> 
+        </button>
+
+         </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+
+      </div>
+      
+
+
+
+      
+      
+      
+
+
+
+      
+      
+
+      
+    </body>
+    </html>
+  
+  </div>
+</template>
